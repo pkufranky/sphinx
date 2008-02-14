pkufranky/sphinx

commit 1a579c694c9412acd6c007ee34d8c5025df75b63

- BuildKeywords
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1144 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/sphinx.cpp
@@ -1060,18 +1060,22 @@ struct CSphTermSetup : ISphNoncopyable
 	ESphDocinfo				m_eDocinfo;
 	CSphDocInfo				m_tMin;
 	int						m_iToCalc;
-	const CSphAutofile &	m_tDoclist;
-	const CSphAutofile &	m_tHitlist;
+	int						m_iDoclistFD;
+	const CSphString &		m_sDoclistFilename;
+	int						m_iHitlistFD;
+	const CSphString &		m_sHitlistFilename;
 	DWORD					m_uMaxStamp;
 
-	CSphTermSetup ( const CSphAutofile & tDoclist, const CSphAutofile & tHitlist )
+	CSphTermSetup ( int iDoclistFD, const CSphString & sDoclistFilename, int iHitlistFD, const CSphString & sHitlistFilename )
 		: m_pTokenizer ( NULL )
 		, m_pDict ( NULL )
 		, m_pIndex ( NULL )
 		, m_eDocinfo ( SPH_DOCINFO_NONE )
 		, m_iToCalc ( 0 )
-		, m_tDoclist ( tDoclist )
-		, m_tHitlist ( tHitlist )
+		, m_iDoclistFD ( iDoclistFD )
+		, m_sDoclistFilename ( sDoclistFilename )
+		, m_iHitlistFD ( iHitlistFD )
+		, m_sHitlistFilename ( sHitlistFilename )
 		, m_uMaxStamp ( 0 )
 	{}
 };
@@ -1542,6 +1546,8 @@ struct CSphIndex_VLN : CSphIndex
 	virtual bool				QueryEx ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery, CSphQueryResult * pResult, ISphMatchSorter * pTop );
 	virtual bool				MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery, CSphQueryResult * pResult, int iSorters, ISphMatchSorter ** ppSorters );
 
+	virtual bool				GetKeywords ( CSphVector <CSphKeywordInfo> & dKeywords, ISphTokenizer * pTokenizer, CSphDict * pDict, const char * szQuery, bool bGetStats );
+
 	virtual bool				Merge( CSphIndex * pSource, CSphPurgeData & tPurgeData );
 	void						MergeWordData ( CSphWordRecord & tDstWord, CSphWordRecord & tSrcWord );
 
@@ -1655,6 +1661,8 @@ private:
 	void						CheckExtendedQuery ( const CSphExtendedQueryNode * pNode, CSphQueryResult * pResult );
 	void						CheckBooleanQuery ( const CSphBooleanQueryExpr * pExpr, CSphQueryResult * pResult );
 
+	CSphDict *					SetupStarDict ( ISphTokenizer * pTokenizer, CSphDict * pDict );
+
 private:
 	static const int MAX_ORDINAL_STR_LEN	= 1024;		///< maximum ordinal string length in bytes
 	static const int ORDINAL_READ_SIZE		= 262144;	///< sorted ordinal id read buffer size in bytes
@@ -1666,7 +1674,7 @@ private:
 
 public:
 	// FIXME! this needs to be protected, and refactored as well
-	bool						SetupQueryWord ( CSphQueryWord & tWord, const CSphTermSetup & tTermSetup );
+	bool						SetupQueryWord ( CSphQueryWord & tWord, const CSphTermSetup & tTermSetup, bool bSetupReaders = true );
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -11915,7 +11923,7 @@ void CSphIndex_VLN::CheckQueryWord ( const char * szWord, CSphQueryResult * pRes
 
 //////////////////////////////////////////////////////////////////////////////
 
-bool CSphIndex_VLN::SetupQueryWord ( CSphQueryWord & tWord, const CSphTermSetup & tTermSetup )
+bool CSphIndex_VLN::SetupQueryWord ( CSphQueryWord & tWord, const CSphTermSetup & tTermSetup, bool bSetupReaders )
 {
 	tWord.m_iDocs = 0;
 	tWord.m_iHits = 0;
@@ -11998,15 +12006,20 @@ bool CSphIndex_VLN::SetupQueryWord ( CSphQueryWord & tWord, const CSphTermSetup 
 		// it matches?!
 		if ( iWordID==tWord.m_iWordID )
 		{
-			// unpack next word ID and offset delta (ie. hitlist length)
-			sphUnzipWordid ( pBuf ); // might be 0 at checkpoint
-			SphOffset_t iDoclistLen = sphUnzipOffset ( pBuf );
-
-			tWord.m_rdDoclist.SetFile ( tTermSetup.m_tDoclist );
-			tWord.m_rdDoclist.SeekTo ( iDoclistOffset, (int)iDoclistLen );
-			tWord.m_rdHitlist.SetFile ( tTermSetup.m_tHitlist );
 			tWord.m_iDocs = iDocs;
 			tWord.m_iHits = iHits;
+
+			if ( bSetupReaders )
+			{
+				// unpack next word ID and offset delta (ie. hitlist length)
+				sphUnzipWordid ( pBuf ); // might be 0 at checkpoint
+				SphOffset_t iDoclistLen = sphUnzipOffset ( pBuf );
+
+				tWord.m_rdDoclist.SetFile ( tTermSetup.m_iDoclistFD, tTermSetup.m_sDoclistFilename.cstr () );
+				tWord.m_rdDoclist.SeekTo ( iDoclistOffset, (int)iDoclistLen );
+				tWord.m_rdHitlist.SetFile ( tTermSetup.m_iHitlistFD, tTermSetup.m_sHitlistFilename.cstr () );
+			}
+
 			return true;
 		}
 	}
@@ -12759,6 +12772,94 @@ bool CSphIndex_VLN::SetupCalc ( CSphQueryResult * pResult, const CSphQuery * pQu
 }
 
 
+CSphDict * CSphIndex_VLN::SetupStarDict  ( ISphTokenizer * pTokenizer, CSphDict * pDict )
+{
+	// setup proper dict
+	bool bUseStarDict = false;
+	if (
+		( m_uVersion>=7 && ( m_iMinPrefixLen>0 || m_iMinInfixLen>0 ) && m_bEnableStar ) || // v.7 added mangling to infixes
+		( m_uVersion==6 && ( m_iMinPrefixLen>0 ) && m_bEnableStar ) ) // v.6 added mangling to prefixes
+	{
+		bUseStarDict = true;
+	}
+
+	CSphDictStar tDictStar ( pDict );
+	CSphDictStarV8 tDictStarV8 ( pDict, m_iMinPrefixLen>0, m_iMinInfixLen>0 );
+	if ( bUseStarDict )
+	{
+		pDict = ( m_uVersion>=8 ) ? &tDictStarV8 : &tDictStar; // v.8 introduced new mangling rules
+
+		CSphRemapRange tStar ( '*', '*', '*' ); // FIXME? check and warn if star was already there
+		pTokenizer->AddCaseFolding ( tStar );
+	}
+
+	return pDict;
+}
+
+
+bool CSphIndex_VLN::GetKeywords ( CSphVector <CSphKeywordInfo> & dKeywords, ISphTokenizer * pTokenizer, CSphDict * pDict, const char * szQuery, bool bGetStats )
+{
+	assert ( pTokenizer );
+	assert ( pDict );
+
+	if ( m_bPreread.IsEmpty() || !m_bPreread[0] )
+	{
+		m_sLastError = "index not preread";
+		return false;
+	}
+
+	CSphScopedPtr <CSphAutofile> pDoclist ( NULL );
+	CSphScopedPtr <CSphAutofile> pHitlist ( NULL );
+
+	pDict = SetupStarDict ( pTokenizer, pDict );
+
+	// prepare for setup
+	CSphTermSetup tTermSetup ( -1, "", -1, "" );
+	tTermSetup.m_pTokenizer = pTokenizer;
+	tTermSetup.m_pDict = pDict;
+	tTermSetup.m_pIndex = this;
+	tTermSetup.m_eDocinfo = m_eDocinfo;
+	tTermSetup.m_tMin = m_tMin;
+
+	dKeywords.Resize ( 0 );
+
+	CSphQueryWord QueryWord;
+	CSphString sTokenized;
+	BYTE * sWord;
+	int nWords = 0;
+
+	CSphString sQbuf ( szQuery );
+	pTokenizer->SetBuffer ( (BYTE*)sQbuf.cstr(), strlen(szQuery) );
+
+	while ( ( sWord = pTokenizer->GetToken() )!=NULL && nWords<SPH_MAX_QUERY_WORDS )
+	{
+		sTokenized = (const char*)sWord;
+
+		SphWordID_t iWord = pDict->GetWordID ( sWord );
+		if ( iWord )
+		{
+			if ( bGetStats )
+			{
+				QueryWord.Reset ();
+				QueryWord.m_sWord = (const char*)sWord;
+				QueryWord.m_iWordID = iWord;
+				SetupQueryWord ( QueryWord, tTermSetup, false );
+			}
+
+			dKeywords.Resize ( dKeywords.GetLength () + 1 );
+			CSphKeywordInfo & tInfo = dKeywords.Last ();
+			Swap ( tInfo.m_sTokenized, sTokenized );
+			tInfo.m_sNormalized = (const char*)sWord;
+			tInfo.m_iDocs = bGetStats ? QueryWord.m_iDocs : 0;
+			tInfo.m_iHits = bGetStats ? QueryWord.m_iHits : 0;
+			++nWords;
+		}
+	}
+
+	return true;
+}
+
+
 bool CSphIndex_VLN::MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery, CSphQueryResult * pResult, int iSorters, ISphMatchSorter ** ppSorters )
 {
 	assert ( pTokenizer );
@@ -12787,31 +12888,14 @@ bool CSphIndex_VLN::MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, C
 	if ( tHitlist.GetFD()<0 )
 		return false;
 
-	// setup proper dict
-	bool bUseStarDict = false;
-	if (
-		( m_uVersion>=7 && ( m_iMinPrefixLen>0 || m_iMinInfixLen>0 ) && m_bEnableStar ) || // v.7 added mangling to infixes
-		( m_uVersion==6 && ( m_iMinPrefixLen>0 ) && m_bEnableStar ) ) // v.6 added mangling to prefixes
-	{
-		bUseStarDict = true;
-	}
-
-	CSphDictStar tDictStar ( pDict );
-	CSphDictStarV8 tDictStarV8 ( pDict, m_iMinPrefixLen>0, m_iMinInfixLen>0 );
-	if ( bUseStarDict )
-	{
-		pDict = ( m_uVersion>=8 ) ? &tDictStarV8 : &tDictStar; // v.8 introduced new mangling rules
-
-		CSphRemapRange tStar ( '*', '*', '*' ); // FIXME? check and warn if star was already there
-		pTokenizer->AddCaseFolding ( tStar );
-	}
+	pDict = SetupStarDict  ( pTokenizer, pDict );
 
 	// setup calculations and result schema
 	if ( !SetupCalc ( pResult, pQuery ) )
 		return false;
 
 	// prepare for setup
-	CSphTermSetup tTermSetup ( tDoclist, tHitlist );
+	CSphTermSetup tTermSetup ( tDoclist.GetFD (), tDoclist.GetFilename (), tHitlist.GetFD (), tHitlist.GetFilename () );
 	tTermSetup.m_pTokenizer = pTokenizer;
 	tTermSetup.m_pDict = pDict;
 	tTermSetup.m_pIndex = this;

api/sphinxapi.php
@@ -21,11 +21,13 @@
 define ( "SEARCHD_COMMAND_SEARCH",	0 );
 define ( "SEARCHD_COMMAND_EXCERPT",	1 );
 define ( "SEARCHD_COMMAND_UPDATE",	2 );
+define ( "SEARCHD_COMMAND_KEYWORDS",3 );
 
 /// current client-side command implementation versions
 define ( "VER_COMMAND_SEARCH",		0x112 );
 define ( "VER_COMMAND_EXCERPT",		0x100 );
 define ( "VER_COMMAND_UPDATE",		0x101 );
+define ( "VER_COMMAND_KEYWORDS",	0x100 );
 
 /// known searchd status codes
 define ( "SEARCHD_OK",				0 );
@@ -992,6 +994,91 @@ class SphinxClient
 			}
 			$res[] = $len ? substr ( $response, $pos, $len ) : "";
 			$pos += $len;
+		}
+
+		$this->_MBPop ();
+		return $res;
+	}
+
+
+	/////////////////////////////////////////////////////////////////////////////
+	// keyword generation
+	/////////////////////////////////////////////////////////////////////////////
+
+	/// connect to searchd server, and generate keyword list for a given query
+	/// returns false on failure,
+	/// an array of words on success
+	function BuildKeywords ( $query, $index, $hits )
+	{
+		assert ( is_string($query) );
+		assert ( is_string($index) );
+		assert ( is_bool($hits) );
+
+		$this->_MBPush ();
+
+		if (!( $fp = $this->_Connect() ))
+		{
+			$this->_MBPop();
+			return false;
+		}
+
+		/////////////////
+		// build request
+		/////////////////
+
+		// v.1.0 req
+		$req  = pack ( "N", strlen($query) ) . $query; // req query
+		$req .= pack ( "N", strlen($index) ) . $index; // req index
+		$req .= pack ( "N", (int)$hits );
+
+		////////////////////////////
+		// send query, get response
+		////////////////////////////
+
+		$len = strlen($req);
+		$req = pack ( "nnN", SEARCHD_COMMAND_KEYWORDS, VER_COMMAND_KEYWORDS, $len ) . $req; // add header
+		$wrote = fwrite ( $fp, $req, $len+8 );
+		if (!( $response = $this->_GetResponse ( $fp, VER_COMMAND_KEYWORDS ) ))
+		{
+			$this->_MBPop ();
+			return false;
+		}
+
+		//////////////////
+		// parse response
+		//////////////////
+
+		$pos = 0;
+		$res = array ();
+		$rlen = strlen($response);
+		list(,$nwords) = unpack ( "N*", substr ( $response, $pos, 4 ) );
+		$pos += 4;
+		for ( $i=0; $i<$nwords; $i++ )
+		{
+			list(,$len) = unpack ( "N*", substr ( $response, $pos, 4 ) );	$pos += 4;
+			$tokenized = $len ? substr ( $response, $pos, $len ) : "";
+			$pos += $len;
+
+			list(,$len) = unpack ( "N*", substr ( $response, $pos, 4 ) );	$pos += 4;
+			$normalized = $len ? substr ( $response, $pos, $len ) : "";
+			$pos += $len;
+
+			$res[] = array ( "tokenized"=>$tokenized, "normalized"=>$normalized );
+
+			if ( $hits )
+			{
+				list($ndocs,$nhits) = array_values ( unpack ( "N*N*", substr ( $response, $pos, 8 ) ) );
+				$pos += 8;
+				$res [$i]["docs"] = $ndocs;
+				$res [$i]["hits"] = $nhits;
+			}
+
+			if ( $pos > $rlen )
+			{
+				$this->_error = "incomplete reply";
+				$this->_MBPop ();
+				return false;
+			}
 		}
 
 		$this->_MBPop ();

src/searchd.cpp
@@ -165,6 +165,7 @@ enum SearchdCommand_e
 	SEARCHD_COMMAND_SEARCH		= 0,
 	SEARCHD_COMMAND_EXCERPT		= 1,
 	SEARCHD_COMMAND_UPDATE		= 2,
+	SEARCHD_COMMAND_KEYWORDS	= 3,
 
 	SEARCHD_COMMAND_TOTAL
 };
@@ -175,7 +176,8 @@ enum
 {
 	VER_COMMAND_SEARCH		= 0x112,
 	VER_COMMAND_EXCERPT		= 0x100,
-	VER_COMMAND_UPDATE		= 0x101
+	VER_COMMAND_UPDATE		= 0x101,
+	VER_COMMAND_KEYWORDS	= 0x100
 };
 
 
@@ -3692,6 +3694,64 @@ void HandleCommandExcerpt ( int iSock, int iVer, InputBuffer_c & tReq )
 }
 
 /////////////////////////////////////////////////////////////////////////////
+// KEYWORDS HANDLER
+/////////////////////////////////////////////////////////////////////////////
+void HandleCommandKeywords ( int iSock, int iVer, InputBuffer_c & tReq )
+{
+	if ( !CheckCommandVersion ( iVer, VER_COMMAND_KEYWORDS, tReq ) )
+		return;
+
+	CSphString sQuery = tReq.GetString ();
+	CSphString sIndex = tReq.GetString ();
+	bool bGetStats = !!tReq.GetInt ();
+
+	const ServedIndex_t * pIndex = g_hIndexes(sIndex);
+	if ( !pIndex )
+	{
+		tReq.SendErrorReply ( "unknown local index '%s' in search request", sIndex.cstr() );
+		return;
+	}
+
+	CSphVector < CSphKeywordInfo > dKeywords;
+	dKeywords.Reserve ( SPH_MAX_QUERY_WORDS );
+
+	if ( !pIndex->m_pIndex->GetKeywords ( dKeywords, pIndex->m_pTokenizer, pIndex->m_pDict, sQuery.cstr (), bGetStats ) )
+	{
+		tReq.SendErrorReply ( "error generating keywords: %s", pIndex->m_pIndex->GetLastError ().cstr () );
+		return;
+	}
+
+	int iRespLen = 4;
+	ARRAY_FOREACH ( i, dKeywords )
+	{
+		iRespLen += 4 + strlen ( dKeywords [i].m_sTokenized.cstr () );
+		iRespLen += 4 + strlen ( dKeywords [i].m_sNormalized.cstr () );
+		if ( bGetStats )
+			iRespLen += 8;
+	}
+
+	NetOutputBuffer_c tOut ( iSock );
+	tOut.SendWord ( SEARCHD_OK );
+	tOut.SendWord ( VER_COMMAND_KEYWORDS );
+	tOut.SendInt ( iRespLen );
+	tOut.SendInt ( dKeywords.GetLength () );
+	ARRAY_FOREACH ( i, dKeywords )
+	{
+		tOut.SendString ( dKeywords [i].m_sTokenized.cstr () );
+		tOut.SendString ( dKeywords [i].m_sNormalized.cstr () );
+		if ( bGetStats )
+		{
+			tOut.SendInt ( dKeywords [i].m_iDocs );
+			tOut.SendInt ( dKeywords [i].m_iHits );
+		}
+	}
+
+	tOut.Flush ();
+	assert ( tOut.GetError()==true || tOut.GetSentCount()==iRespLen+8 );
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
 // UPDATES HANDLER
 /////////////////////////////////////////////////////////////////////////////
 
@@ -4005,6 +4065,7 @@ void HandleClient ( int iSock, const char * sClientIP, int iPipeFD )
 	{
 		case SEARCHD_COMMAND_SEARCH:	SafeClose ( iPipeFD ); HandleCommandSearch ( iSock, iCommandVer, tBuf ); break;
 		case SEARCHD_COMMAND_EXCERPT:	SafeClose ( iPipeFD ); HandleCommandExcerpt ( iSock, iCommandVer, tBuf ); break;
+		case SEARCHD_COMMAND_KEYWORDS:	SafeClose ( iPipeFD ); HandleCommandKeywords ( iSock, iCommandVer, tBuf ); break;
 		case SEARCHD_COMMAND_UPDATE:	HandleCommandUpdate ( iSock, iCommandVer, tBuf, iPipeFD ); SafeClose ( iPipeFD ); break;
 		default:						assert ( 0 && "INTERNAL ERROR: unhandled command" ); break;
 	}

src/sphinx.h
@@ -1340,6 +1340,16 @@ protected:
 };
 
 
+// keyword info
+struct CSphKeywordInfo
+{
+	CSphString		m_sTokenized;
+	CSphString		m_sNormalized;
+	int				m_iDocs;
+	int				m_iHits;
+};
+
+
 /// name+int pair
 struct CSphNamedInt
 {
@@ -1692,6 +1702,7 @@ public:
 	virtual CSphQueryResult *	Query ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery ) = 0;
 	virtual bool				QueryEx ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery, CSphQueryResult * pResult, ISphMatchSorter * pTop ) = 0;
 	virtual bool				MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphQuery * pQuery, CSphQueryResult * pResult, int iSorters, ISphMatchSorter ** ppSorters ) = 0;
+	virtual bool				GetKeywords ( CSphVector <CSphKeywordInfo> & dKeywords, ISphTokenizer * pTokenizer, CSphDict * pDict, const char * szQuery, bool bGetStats ) = 0;
 
 public:
 	/// updates memory-cached attributes in real time

commit 0f7c0806d132f4a4f38a22990e177c51f377fac1

fixed crashes due to returning local var from SetupStarDict() (broken since r1144)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1148 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/sphinx.cpp
@@ -1661,7 +1661,7 @@ private:
 	void						CheckExtendedQuery ( const CSphExtendedQueryNode * pNode, CSphQueryResult * pResult );
 	void						CheckBooleanQuery ( const CSphBooleanQueryExpr * pExpr, CSphQueryResult * pResult );
 
-	CSphDict *					SetupStarDict ( ISphTokenizer * pTokenizer, CSphDict * pDict );
+	CSphDict *					SetupStarDict ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphScopedPtr<CSphDict> & tContainer );
 
 private:
 	static const int MAX_ORDINAL_STR_LEN	= 1024;		///< maximum ordinal string length in bytes
@@ -12773,7 +12773,7 @@ bool CSphIndex_VLN::SetupCalc ( CSphQueryResult * pResult, const CSphQuery * pQu
 }
 
 
-CSphDict * CSphIndex_VLN::SetupStarDict  ( ISphTokenizer * pTokenizer, CSphDict * pDict )
+CSphDict * CSphIndex_VLN::SetupStarDict  ( ISphTokenizer * pTokenizer, CSphDict * pDict, CSphScopedPtr<CSphDict> & tContainer )
 {
 	// setup proper dict
 	bool bUseStarDict = false;
@@ -12784,17 +12784,21 @@ CSphDict * CSphIndex_VLN::SetupStarDict  ( ISphTokenizer * pTokenizer, CSphDict 
 		bUseStarDict = true;
 	}
 
-	CSphDictStar tDictStar ( pDict );
-	CSphDictStarV8 tDictStarV8 ( pDict, m_iMinPrefixLen>0, m_iMinInfixLen>0 );
-	if ( bUseStarDict )
-	{
-		pDict = ( m_uVersion>=8 ) ? &tDictStarV8 : &tDictStar; // v.8 introduced new mangling rules
+	// no star? just return the original one
+	if ( !bUseStarDict )
+		return pDict;
 
-		CSphRemapRange tStar ( '*', '*', '*' ); // FIXME? check and warn if star was already there
-		pTokenizer->AddCaseFolding ( tStar );
-	}
+	// spawn wrapper, and put it in the box
+	// wrapper type depends on version; v.8 introduced new mangling rules
+	if ( m_uVersion>=8 )
+		tContainer = new CSphDictStarV8 ( pDict, m_iMinPrefixLen>0, m_iMinInfixLen>0 );
+	else
+		tContainer = new CSphDictStar ( pDict );
 
-	return pDict;
+	CSphRemapRange tStar ( '*', '*', '*' ); // FIXME? check and warn if star was already there
+	pTokenizer->AddCaseFolding ( tStar );
+
+	return tContainer.Ptr();
 }
 
 
@@ -12812,7 +12816,8 @@ bool CSphIndex_VLN::GetKeywords ( CSphVector <CSphKeywordInfo> & dKeywords, ISph
 	CSphScopedPtr <CSphAutofile> pDoclist ( NULL );
 	CSphScopedPtr <CSphAutofile> pHitlist ( NULL );
 
-	pDict = SetupStarDict ( pTokenizer, pDict );
+	CSphScopedPtr<CSphDict> tDict ( NULL );
+	pDict = SetupStarDict ( pTokenizer, pDict, tDict );
 
 	// prepare for setup
 	CSphTermSetup tTermSetup ( -1, "", -1, "" );
@@ -12889,7 +12894,8 @@ bool CSphIndex_VLN::MultiQuery ( ISphTokenizer * pTokenizer, CSphDict * pDict, C
 	if ( tHitlist.GetFD()<0 )
 		return false;
 
-	pDict = SetupStarDict  ( pTokenizer, pDict );
+	CSphScopedPtr<CSphDict> tDict ( NULL );
+	pDict = SetupStarDict  ( pTokenizer, pDict, tDict );
 
 	// setup calculations and result schema
 	if ( !SetupCalc ( pResult, pQuery ) )