pkufranky/sphinx

commit 976f149f4e9ab17b45cb6ee1341f059ffbae1f75

fixed overshort keywords vs. phrase matching (both normal and extended)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1171 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/sphinx.cpp
@@ -2715,6 +2715,7 @@ ISphTokenizer::ISphTokenizer ()
 	, m_bBoundary ( false )
 	, m_bWasSpecial ( false )
 	, m_bEscaped ( false )
+	, m_iOvershortCount ( 0 )
 {}
 
 
@@ -3405,7 +3406,11 @@ BYTE * CSphTokenizerTraits<IS_UTF8>::GetTokenSyn ()
 
 		// return accumulated token
 		if ( m_iAccum<m_iMinWordLen )
+		{
+			if ( m_iAccum )
+				m_iOvershortCount++;
 			continue;
+		}
 
 		*m_pAccum = '\0';
 		m_iLastTokenLen = m_iAccum;
@@ -3484,6 +3489,26 @@ bool ISphTokenizer::SetIgnoreChars ( const char * sConfig, CSphString & sError )
 	return true;
 }
 
+
+void ISphTokenizer::CloneBase ( const ISphTokenizer * pFrom, bool bEscaped )
+{
+	m_tLC = pFrom->m_tLC;
+	m_iMinWordLen = pFrom->m_iMinWordLen;
+	m_dSynonyms = pFrom->m_dSynonyms;
+	m_dSynStart = pFrom->m_dSynStart;
+	m_dSynEnd = pFrom->m_dSynEnd;
+	m_bEscaped = bEscaped;
+
+	if ( bEscaped )
+	{
+		CSphVector<CSphRemapRange> dRemaps;
+		CSphRemapRange Range;
+		Range.m_iStart = Range.m_iEnd = Range.m_iRemapStart = '\\';
+		dRemaps.Add ( Range );
+		m_tLC.AddRemaps ( dRemaps, 0, 0 );
+	}
+}
+
 /////////////////////////////////////////////////////////////////////////////
 
 CSphTokenizer_SBCS::CSphTokenizer_SBCS ()
@@ -3502,12 +3527,15 @@ void CSphTokenizer_SBCS::SetBuffer ( BYTE * sBuffer, int iLength )
 	m_pBuffer = sBuffer;
 	m_pBufferMax = sBuffer + iLength;
 	m_pCur = sBuffer;
+
+	m_iOvershortCount = 0;
 }
 
 
 BYTE * CSphTokenizer_SBCS::GetToken ()
 {
 	m_bWasSpecial = false;
+	m_iOvershortCount = 0;
 
 	if ( m_dSynonyms.GetLength() )
 		return GetTokenSyn ();
@@ -3560,6 +3588,8 @@ BYTE * CSphTokenizer_SBCS::GetToken ()
 		{
 			if ( m_iAccum<m_iMinWordLen )
 			{
+				if ( m_iAccum )
+					m_iOvershortCount++;
 				m_iAccum = 0;
 				continue;
 			}
@@ -3581,7 +3611,11 @@ BYTE * CSphTokenizer_SBCS::GetToken ()
 		{
 			// skip short words
 			if ( m_iAccum<m_iMinWordLen )
+			{
 				m_iAccum = 0;
+				if ( m_iAccum )
+					m_iOvershortCount++;
+			}
 
 			if ( m_iAccum==0 )
 			{
@@ -3613,21 +3647,7 @@ BYTE * CSphTokenizer_SBCS::GetToken ()
 ISphTokenizer * CSphTokenizer_SBCS::Clone ( bool bEscaped ) const
 {
 	CSphTokenizer_SBCS * pClone = new CSphTokenizer_SBCS ();
-	pClone->m_tLC = m_tLC;
-	pClone->m_dSynonyms = m_dSynonyms;
-	pClone->m_dSynStart = m_dSynStart;
-	pClone->m_dSynEnd = m_dSynEnd;
-	pClone->m_bEscaped = bEscaped;
-
-	if ( bEscaped )
-	{
-		CSphVector<CSphRemapRange> dRemaps;
-		CSphRemapRange Range;
-		Range.m_iStart = Range.m_iEnd = Range.m_iRemapStart = '\\';
-		dRemaps.Add ( Range );
-		pClone->m_tLC.AddRemaps ( dRemaps, 0, 0 );
-	}
-
+	pClone->CloneBase ( this, bEscaped );
 	return pClone;
 }
 
@@ -3654,12 +3674,15 @@ void CSphTokenizer_UTF8::SetBuffer ( BYTE * sBuffer, int iLength )
 	for ( BYTE * p = m_pBuffer; p < m_pBufferMax; p++ )
 		if ( !*p )
 			*p = ' ';
+
+	m_iOvershortCount = 0;
 }
 
 
 BYTE * CSphTokenizer_UTF8::GetToken ()
 {
 	m_bWasSpecial = false;
+	m_iOvershortCount = 0;
 
 	if ( m_dSynonyms.GetLength() )
 		return GetTokenSyn ();
@@ -3716,8 +3739,11 @@ BYTE * CSphTokenizer_UTF8::GetToken ()
 		{
 			FlushAccum ();
 			if ( m_iLastTokenLen<m_iMinWordLen )
+			{
+				if ( m_iLastTokenLen )
+					m_iOvershortCount++;
 				continue;
-			else
+			} else
 				return m_sAccum;
 		}
 
@@ -3732,7 +3758,11 @@ BYTE * CSphTokenizer_UTF8::GetToken ()
 		{
 			// skip short words preceding specials
 			if ( m_iAccum<m_iMinWordLen )
+			{
+				if ( m_iAccum )
+					m_iOvershortCount++;
 				FlushAccum ();
+			}
 
 			if ( m_iAccum==0 )
 			{
@@ -3766,21 +3796,7 @@ void CSphTokenizer_UTF8::FlushAccum ()
 ISphTokenizer * CSphTokenizer_UTF8::Clone ( bool bEscaped ) const
 {
 	CSphTokenizer_UTF8 * pClone = new CSphTokenizer_UTF8 ();
-	pClone->m_tLC = m_tLC;
-	pClone->m_dSynonyms = m_dSynonyms;
-	pClone->m_dSynStart = m_dSynStart;
-	pClone->m_dSynEnd = m_dSynEnd;
-	pClone->m_bEscaped = bEscaped;
-
-	if ( bEscaped )
-	{
-		CSphVector<CSphRemapRange> dRemaps;
-		CSphRemapRange Range;
-		Range.m_iStart = Range.m_iEnd = Range.m_iRemapStart = '\\';
-		dRemaps.Add ( Range );
-		pClone->m_tLC.AddRemaps ( dRemaps, 0, 0 );
-	}
-
+	pClone->CloneBase ( this, bEscaped );
 	return pClone;
 }
 
@@ -8053,14 +8069,15 @@ struct CSphSimpleQueryParser
 			SphWordID_t iWord = pDict->GetWordID ( sWord );
 			if ( iWord )
 			{
+				iPos += 1+pTokenizer->GetOvershortCount(); // properly advance position
 				m_dWords[iWords].m_sWord = (const char*)sWord;
 				m_dWords[iWords].m_uWordID = iWord;
-				m_dWords[iWords].m_iQueryPos = ++iPos;
-				++iWords;
+				m_dWords[iWords].m_iQueryPos = iPos;
+				iWords++;
 			} else
 			{
 				if ( iPos )
-					++iPos;
+					iPos++; // advance position on stopwords, but not at the very start
 			}
 		}
 
@@ -15249,7 +15266,7 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 		m_pTokenizer->SetBuffer ( sField, iFieldBytes );
 
 		BYTE * sWord;
-		int iPos = ( j<<24 ) + 1;
+		int iPos = ( j<<24 );
 
 		bool bPrefixField = m_tSchema.m_dFields [j].m_eWordpart == SPH_WORDPART_PREFIX;
 		bool bInfixMode = m_iMinInfixLen > 0;
@@ -15265,6 +15282,7 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 			{
 				if ( m_pTokenizer->GetBoundary() )
 					iPos += m_iBoundaryStep;
+				iPos += 1+m_pTokenizer->GetOvershortCount();
 
 				int iLen = m_pTokenizer->GetLastTokenLen ();
 
@@ -15318,8 +15336,6 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 						tHit.m_iWordID = iWord;
 						tHit.m_iWordPos = iPos;
 					}
-
-					iPos++;
 					continue;
 				}
 
@@ -15377,9 +15393,6 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 
 					sInfix += m_pTokenizer->GetCodepointLength ( *sInfix );
 				}
-
-				// move on
-				iPos++; 
 			}
 		} else
 		{
@@ -15388,6 +15401,7 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 			{
 				if ( m_pTokenizer->GetBoundary() )
 					iPos += m_iBoundaryStep;
+				iPos += 1+m_pTokenizer->GetOvershortCount();
 
 				if ( bGlobalPartialMatch )
 				{
@@ -15414,7 +15428,6 @@ bool CSphSource_Document::IterateHitsNext ( CSphString & sError )
 					tHit.m_iWordID = iWord;
 					tHit.m_iWordPos = iPos;
 				}
-				iPos++;
 			}
 		}
 	}

src/sphinx.h
@@ -390,12 +390,18 @@ public:
 	/// was last token a special one?
 	inline bool						WasTokenSpecial () { return m_bWasSpecial; }
 
+	/// get amount of overshort keywords skipped before this token
+	inline int						GetOvershortCount () { return m_iOvershortCount; }
+
 public:
 	/// get lowercaser
 	virtual const CSphLowercaser *	GetLowercaser () const { return &m_tLC; }
 
 	/// spawn a clone of my own
 	virtual ISphTokenizer *			Clone ( bool bEscaped ) const = 0;
+
+	/// clone base stuff
+	virtual void					CloneBase ( const ISphTokenizer * pFrom, bool bEscaped );
 
 	/// SBCS or UTF-8?
 	virtual bool					IsUtf8 () const = 0;
@@ -419,6 +425,7 @@ protected:
 	bool							m_bBoundary;				///< boundary flag (true immediately after boundary codepoint)
 	bool							m_bWasSpecial;				///< special token flag
 	bool							m_bEscaped;					///< backslash handling flag
+	int								m_iOvershortCount;			///< skipped overshort tokens count
 
 	CSphVector<CSphSynonym>			m_dSynonyms;				///< active synonyms
 	CSphVector<int>					m_dSynStart;				///< map 1st byte to candidate range start

src/sphinxquery.cpp
@@ -1076,18 +1076,18 @@ bool CSphExtendedQueryParser::Parse ( CSphExtendedQuery & tParsed, const char * 
 			: QUERY_END;
 		assert ( !( iSpecial>0 && sToken[1]!=0 ) );
 
-		// ignore stopwords
+		// ignore stopwords, update positions
 		if ( !iSpecial )
 		{
 			// GetWordID() may modify the word in-place; so we alloc a tempbuffer
 			CSphString sTmp ( sToken );
 			SphWordID_t iWordID = pDict->GetWordID ( (BYTE*)sTmp.cstr() );
-			if ( iWordID==0 )
-			{
-				if ( iAtomPos )
-					iAtomPos++;
+
+			if ( iWordID || iAtomPos ) // fully ignore starting stopwords; update positions for everything else
+				iAtomPos += 1+pMyTokenizer->GetOvershortCount();
+
+			if ( !iWordID ) // skip stopwords
 				continue;
-			}
 		}
 
 		///////////////////////////
@@ -1149,7 +1149,7 @@ bool CSphExtendedQueryParser::Parse ( CSphExtendedQuery & tParsed, const char * 
 			if ( dState.Last()==XQS_PHRASE )
 			{
 				assert ( m_dStack.Last().m_pNode->m_tAtom.m_iMaxDistance==0 );
-				CSphExtendedQueryAtomWord tAW ( sToken, iAtomPos++ );
+				CSphExtendedQueryAtomWord tAW ( sToken, iAtomPos );
 				m_dStack.Last().m_pNode->m_tAtom.m_dWords.Add ( tAW );
 				continue;
 			}

test/test_17/model.bin
@@ -1,0 +1,1 @@
+a:1:{i:0;a:8:{i:0;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:1;a:2:{s:6:"weight";s:1:"2";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.001";s:5:"words";a:2:{s:7:"walking";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:5:"shoes";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:13:"walking shoes";}i:1;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:2;a:2:{s:6:"weight";s:1:"2";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.001";s:5:"words";a:2:{s:7:"walking";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:5:"shoes";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:19:"walking in my shoes";}i:2;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:4;a:2:{s:6:"weight";s:1:"2";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:9:"microsoft";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:6:"office";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:16:"microsoft office";}i:3;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:3;a:2:{s:6:"weight";s:1:"2";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:9:"microsoft";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:6:"office";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:18:"microsoft a office";}i:4;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:1;a:2:{s:6:"weight";s:4:"2557";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:7:"walking";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:5:"shoes";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:15:""walking shoes"";}i:5;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:2;a:2:{s:6:"weight";s:4:"4557";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:7:"walking";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:5:"shoes";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:21:""walking in my shoes"";}i:6;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:4;a:2:{s:6:"weight";s:4:"2557";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:9:"microsoft";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:6:"office";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:18:""microsoft office"";}i:7;a:11:{s:5:"error";s:0:"";s:7:"warning";s:0:"";s:6:"status";i:0;s:6:"fields";a:1:{i:0;s:4:"body";}s:5:"attrs";a:0:{}s:7:"matches";a:1:{i:3;a:2:{s:6:"weight";s:4:"3557";s:5:"attrs";a:0:{}}}s:5:"total";s:1:"1";s:11:"total_found";s:1:"1";s:4:"time";s:5:"0.002";s:5:"words";a:2:{s:9:"microsoft";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}s:6:"office";a:2:{s:4:"docs";s:1:"2";s:4:"hits";s:1:"2";}}s:5:"query";s:20:""microsoft a office"";}}}

test/test_17/test.xml
@@ -1,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<test>
+
+<name>phrase matching vs. stop words and short words</name>
+
+<config>
+indexer
+{
+	mem_limit		= 16M
+}
+
+searchd
+{
+	<searchd_settings/>
+}
+
+source srctest
+{
+	type			= mysql
+	<sql_settings/>
+	sql_query		= SELECT document_id, body FROM test_table
+}
+
+index test
+{
+	source			= srctest
+	path			= <data_path/>/test
+	stopwords		= stopwords.txt
+	min_word_len	= 3
+}
+</config>
+
+<queries>
+<query mode="phrase">walking shoes</query>
+<query mode="phrase">walking in my shoes</query>
+<query mode="phrase">microsoft office</query>
+<query mode="phrase">microsoft a office</query>
+<query mode="extended2">"walking shoes"</query>
+<query mode="extended2">"walking in my shoes"</query>
+<query mode="extended2">"microsoft office"</query>
+<query mode="extended2">"microsoft a office"</query>
+</queries>
+
+<db_create>
+CREATE TABLE `test_table`
+(
+	`document_id` int(11) NOT NULL default '0',
+	`body` varchar(255) NOT NULL default ''
+)
+</db_create>
+
+<db_drop>
+DROP TABLE IF EXISTS `test_table`
+</db_drop>
+
+<db_insert>
+INSERT INTO `test_table` VALUES
+( 1, 'walking shoes' ),
+( 2, 'try walking in my shoes' ),
+( 3, 'Microsoft. The Office.' ),
+( 4, 'Microsoft Office' )
+</db_insert>
+
+</test>

commit 3cd4495f0c769c1c0d43d61937ee1e27e67ade48

fixed extended1 vs phrase queries (broken in r1171)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@1201 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/sphinx.cpp
@@ -9472,7 +9472,7 @@ void CSphExtendedEvalAtom::GetNextHit ( DWORD iMinPos )
 				continue;
 
 			// scan forward until this hit is not too early
-			const DWORD uRequiredPos = uCandidate + m_dWords[i].m_iAtomPos;
+			const DWORD uRequiredPos = uCandidate + m_dWords[i].m_iAtomPos - 1;
 			while ( uPos < uRequiredPos )
 			{
 				// if one hitlist is over, document is over, because all words must be in the phrase