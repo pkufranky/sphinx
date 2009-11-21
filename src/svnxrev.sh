pkufranky/sphinx

commit 6daf7847bfa1b82529188fbdfbe28c8aaaf88be1

fixed svnxrev to use.. more regular shell stuff (bug #436)
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@2091 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/svnxrev.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 ## As standalone run from this directory as "./svnxrev.sh .."
 
-if [ -e $1/.svn ] ; then 
+if [ -d $1/.svn ] ; then 
     svn info --xml $1 | perl $1/src/svnxrev.pl $1/src/sphinxversion.h
-elif [ -e $1/.hg ] ; then
-    ddr=$(pwd)
+elif [ -d $1/.hg ] ; then
+    ddr=`pwd`
     cd $1
 	target="src/sphinxversion.h"
-	startrev=$(hg id -n)
+	startrev=`hg id -n`
 	rev="$startrev"
-	svnrev=$(hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//')
+	svnrev=`hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//'`
 	while [ "z" = "z$svnrev" ] ; do
-		rev=$(($(hg log -r$rev --template "{rev}")-1))
-		svnrev=$(hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//')
+		rev=`hg log -r$rev --template "{rev}"`-1
+		svnrev=`hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//'`
 	done
 	rsvnrev="r$svnrev"
-	node=$(hg log -r$startrev --template "{node}")
-	branch=$(hg log -r$startrev --template "{branches}")
+	node=`hg log -r$startrev --template "{node}"`
+	branch=`hg log -r$startrev --template "{branches}"`
 	[ "z$branch" = "ztrunk" ] && branch=""
 	[ ! "z$startrev" = "z$rev" ]  && rsvnrev=r"$svnrev/$node"
 	printf "#define SPH_SVN_TAG \"%s\"\n" $branch > $target
@@ -28,4 +28,3 @@ fi
 if [ ! -f ./sphinxversion.h ] ; then
     cat *.cpp *.h | perl srcxrev.pl > sphinxversion.h
 fi
-

commit 1db8109329acba252dfaf71185c3285e39007666

svnxrev.sh corrected for mercurial (was broken in r2091), keeping solaris' sh happy.
git-svn-id: http://sphinxsearch.googlecode.com/svn/trunk@2092 3dc63b7a-4a1c-11de-898f-4722afa3338a

src/svnxrev.sh
@@ -7,12 +7,13 @@ elif [ -d $1/.hg ] ; then
     ddr=`pwd`
     cd $1
 	target="src/sphinxversion.h"
-	startrev=`hg id -n`
+	startrev=`hg id -n | sed 's/\\+//'`
 	rev="$startrev"
-	svnrev=`hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//'`
+	svnrev=`hg log -r$rev --template "{desc}" | grep ^\\\\[svn | sed 's/\\[svn r//; s/\\].*//'`
 	while [ "z" = "z$svnrev" ] ; do
-		rev=`hg log -r$rev --template "{rev}"`-1
-		svnrev=`hg log -r$rev --template "{desc}" | grep ^\\[svn | sed 's/\[svn r//; s/\].*//'`
+		revplus=`hg log -r$rev --template "{rev}"`
+		rev=`expr $revplus - 1`
+		svnrev=`hg log -r$rev --template "{desc}" | grep ^\\\\[svn | sed 's/\\[svn r//; s/\\].*//'`
 	done
 	rsvnrev="r$svnrev"
 	node=`hg log -r$startrev --template "{node}"`